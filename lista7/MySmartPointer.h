5d4261a3b6940bb10905daf22233e6e850e00c8b
Szymon Skarzyński <szymon3301@gmail.com>
2019-12-02 12:34:09 +0100

    MySmartPointer changed to template

--- lista7/MySmartPointer.h
+++ lista7/MySmartPointer.h
@@ -2,16 +2,49 @@
 
 #include "RefCounter.h"
 
-class MySmartPointer {
+template <typename T> class MySmartPointer {
 private:
 	RefCounter* counter;
-	int* data;
+	T* data;
 
 public:
-	MySmartPointer(int* data);
+	MySmartPointer(T* data);
 	MySmartPointer(const MySmartPointer& otherPointer);
 	~MySmartPointer();
 
-	int& operator *();
-	int* operator ->();
+	T& operator *();
+	T* operator ->();
 };
+
+//-----------------------------------------------------------------------------------------------
+template <typename T>
+MySmartPointer<T>::MySmartPointer(T* data) {
+	this->data = data;
+	this->counter = new RefCounter();
+	this->counter->add();
+}
+
+template <typename T>
+MySmartPointer<T>::MySmartPointer(const MySmartPointer& otherPointer) {
+	this->data = otherPointer.data;
+	this->counter = otherPointer.counter;
+	this->counter->add();
+}
+
+template <typename T>
+MySmartPointer<T>::~MySmartPointer() {
+	if (this->counter->dec == 0) {
+		delete this->data;
+		delete this->counter;
+	}
+}
+
+template <typename T>
+T& MySmartPointer<T>::operator*() {
+	return *this->data;
+}
+
+template <typename T>
+T* MySmartPointer<T>::operator->() {
+	return this->data;
+}
